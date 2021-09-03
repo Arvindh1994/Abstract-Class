5fe8f095bc9660f44290eee5c438031e26f6c403
Arvindh1994 <arvindharvi94@gmail.com>
2021-09-03 14:49:04 +0530

    My First push

---
+++ src/org/emp/Company.java
@@ -0,0 +1,22 @@
+package org.emp;
+
+public class Company extends Employee {
+	
+	public static void main(String[] args) {
+		
+		empId();
+		Company c = new Company();
+		c.empName();
+		
+		
+		
+	}
+
+	@Override
+	public void empMobile() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
---
+++ src/org/emp/Employee.java
@@ -0,0 +1,32 @@
+package org.emp;
+
+public abstract class Employee {
+	
+  public static void empId() {
+	  
+	  System.out.println("emp id is 888");
+	  
+	
+	}
+  
+  public void empName() {
+	System.out.println("arvindh");
+}
+  public Employee() {
+	  
+	  System.out.println("default constractor");
+	  
+	}
+  public abstract void empMobile();
+ 
+
+}
+  
+  
+      
+      
+      
+  
+	
+
+}
